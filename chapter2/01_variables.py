cf87dbc917363c0061bb5fbca5cc8ffb10be3f24
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-05-19 20:36:27 +0530

    Day 2 task variables and data types

---
+++ chapter2/01_variables.py
@@ -0,0 +1,14 @@
+# interger variable
+a = 1
+
+# Floating point variable
+b = 5.5
+
+# String variable
+c = "Rajnish"
+
+print(a + b)
+
+print(c)
+
+# Variable is something that stores the value of different data types
---
+++ chapter2/02_datatypes.py
@@ -0,0 +1,12 @@
+# data types
+
+a = 1  # a is an integer that stores integer value
+
+b = 5.33  # b is a floating point that stores decimal number
+
+c = "Rajnish"  # c is a string that stores character and words
+
+d = False  # d is a boolean variable that stores true and false value in other words it stores 0 and 1
+
+e = None  # e is an empty variable that stores nothing it just got assigned and it is empty
+
