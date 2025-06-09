386747835d54ed8fa56202197493ea1592d3b2b6
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-06-10 00:08:49 +0530

    chapter 9 bug fixed

--- chapter9/practice6.py
+++ chapter9/practice6.py
@@ -3,10 +3,12 @@
 word = "python"
 i = 1
 with open("log.txt", "r") as f:
-    lines = f.readlines()
+    lines = f.readlines()  # it will return list of lines
 
 for line in lines:
     if "python" in line:
         print(f"python word found in line number {i}")
+        break
+    i += 1
 else:
     print("No python is not present")
