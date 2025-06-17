b7f243f688f410e2d958049433e710c45143c119
Rajnish Kumar <rajnishroushan2020@gmail.com>
2025-06-17 19:55:43 +0530

    Practice set added in chapter 12

---
+++ chapter12/practice02.py
@@ -0,0 +1,6 @@
+l = [1, 2, 1, 5, 4, 1, 515, 1, 5154, 1, 1, 121, 545, 5114]
+
+for index, item in enumerate(l):
+    if index == 2 or index == 4 or index == 6:
+        print(f"The {index} item is {item}")
+    
